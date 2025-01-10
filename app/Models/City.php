4c0fb51469819da487788d5f7b42a3e66e7b17c8
Baklajik <denisdenis20050315@gmail.com>
2025-01-10 15:06:11 +0300

    city and country

--- app/Models/City.php
+++ app/Models/City.php
@@ -4,9 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Requests\CityRequest;
 
 class City extends Model
 {
     /** @use HasFactory<\Database\Factories\CityFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'country_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'country_id' => 'integer',
+        ];
+    }
+
+    public function country(){
+        return $this->belongsTo(Country::class);
+    }
+
+    public static function createCity(CityRequest $request){
+        $data = $request->validated();
+
+        return self::query() ->create($data);
+    }
+
+    public static function updateCity(CityRequest $request, self $city){
+        $data = $request->validated();
+
+        return $city ->update($data);
+    }
+
+    public static function deleteCity(self $city){
+        return $city ->delete();
+    }
 }
