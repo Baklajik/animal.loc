c0b6b0dfe3bef4799e089635aeff461d50d1596b
Baklajik <denisdenis20050315@gmail.com>
2025-01-10 14:35:30 +0300

    1

---
+++ app/Enum/Role.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enum;
+
+use Illuminate\Support\Collection;
+
+enum Role: string
+{
+    case ADMIN = 'admin';
+    case EMPLOYEE = 'employee';
+    case USER = 'user';
+    public static function getValues(): Collection{
+        return collect(Role::cases())->pluck('value');
+    }
+    public function getTitle(){
+        return match ($this){
+            self::ADMIN => 'Администратор',
+            self::EMPLOYEE => 'Работник',
+            self::USER => 'Заявитель',
+        };
+    }
+}
